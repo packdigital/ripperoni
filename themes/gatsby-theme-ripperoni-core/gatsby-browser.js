8a6700f9f3a49e6c07ae66bc0da4f60f476624bb
tshamz <tyler@theshamboras.com>
2020-07-27 13:13:20 -0700

    wrap page element with empty layout in gatsby-theme-core and update error logging when downloading remote image

--- themes/gatsby-theme-ripperoni-core/gatsby-browser.js
+++ themes/gatsby-theme-ripperoni-core/gatsby-browser.js
@@ -6,5 +6,6 @@ require('./src/assets/styles/global.css');
 require('./src/assets/styles/third-party-overwrites.css');
 
 // Browser API hooks
-// exports.replaceHydrateFunction = require('./gatsby/browser/replace-hydrate-function');
+exports.wrapPageElement = require('./gatsby/browser/wrap-page-element');
 exports.onClientEntry = require('./gatsby/browser/on-client-entry');
+// exports.replaceHydrateFunction = require('./gatsby/browser/replace-hydrate-function');
