e194be174da37389597e84a1a8dda58e64d80a78
tshamz <tyler@theshamboras.com>
2020-09-16 13:52:02 -0700

    remove validate pattern from input

--- themes/gatsby-theme-ripperoni-account/src/components/AddressForm/AddressFormPhoneInput.jsx
+++ themes/gatsby-theme-ripperoni-account/src/components/AddressForm/AddressFormPhoneInput.jsx
@@ -4,7 +4,7 @@ import { Input } from 'theme-ui';
 
 export const PhoneInput = props => (
   <Input
-    pattern='[\d\s\.\-\+\(\)]'
+    // pattern='[\d\s\.\-\+\(\)]'
     // pattern='[\d\s\.\-\+\(\)]{10,}'
     // minlength='10'
     {...props}
