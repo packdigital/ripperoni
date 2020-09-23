a12560885a5d731fc313137aa6a1901ffc4a5f25
Andrew Nguyen <anguy95@gmail.com>
2020-09-23 12:09:28 -0700

    feat: make live inventory hook

--- themes/gatsby-theme-ripperoni-store/gatsby-ssr.js
+++ themes/gatsby-theme-ripperoni-store/gatsby-ssr.js
@@ -1,14 +1,14 @@
-const React = require('react');
-const { ApolloProvider } = require('@apollo/react-hooks');
+// const React = require('react');
+// const { ApolloProvider } = require('@apollo/react-hooks');
 
-const { InventoryContextProvider } = require('./src/context/InventoryContext');
-const client = require('./src/api/backpack');
+// const { InventoryContextProvider } = require('./src/context/InventoryContext');
+// const client = require('./src/api/backpack');
 
-// SSR API hooks
-exports.wrapRootElement = ({ element }) => (
-  <ApolloProvider client={client}>
-    <InventoryContextProvider>
-      {element}
-    </InventoryContextProvider>
-  </ApolloProvider>
-);
+// // SSR API hooks
+// exports.wrapRootElement = ({ element }) => (
+//   <ApolloProvider client={client}>
+//     <InventoryContextProvider>
+//       {element}
+//     </InventoryContextProvider>
+//   </ApolloProvider>
+// );
---
+++ themes/gatsby-theme-ripperoni-store/src/hooks/useInventory.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useSubscription } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+import client from '../api/backpack';
+
+export const useInventory = (id) => {
+  const [inventory, setInventory] = useState(null);
+
+  useSubscription(GET_INVENTORY, {
+    variables: {
+      id: parseInt(id.split('__').pop())
+    },
+    client,
+    onSubscriptionData: ({subscriptionData}) => {
+      setInventory(subscriptionData.data.productVariant);
+    },
+  });
+
+  return inventory;
+};
+
+const GET_INVENTORY = gql`
+  subscription Inventory ($id: bigint!) {
+    productVariant(id: $id) {
+      id
+      productId
+      foreignId
+      inventory
+      available
+      foreignProductHandle
+    }
+  }
+`;
