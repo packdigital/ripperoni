25dc20607cee69889648c41f0d565df5930679dd
Andrew Nguyen <anguy95@gmail.com>
2020-09-23 13:55:08 -0700

    fix: modal component order

--- themes/gatsby-theme-ripperoni-components/src/components/Modal/Modal.jsx
+++ themes/gatsby-theme-ripperoni-components/src/components/Modal/Modal.jsx
@@ -17,45 +17,44 @@ export const Modal = ({modalId, children}) => {
   };
 
   return (
-    <React.Fragment>
-      <AnimatePresence>
-        {uiState.modal && (
-          <Overlay onClick={toggleModal} />
-        )}
-      </AnimatePresence>
+    uiState.modal === modalId ? (
+      <React.Fragment>
+        <AnimatePresence>
+          {uiState.modal && (
+            <Overlay onClick={toggleModal} />
+          )}
+        </AnimatePresence>
 
-      {
-        uiState.modal === modalId ? (
+        <Box
+          p={8}
+          position='fixed'
+          top='50%'
+          left='50%'
+          transform='translate(-50%, -50%)'
+          zIndex='2'
+          maxWidth='960px'
+          width='100%'
+        >
           <Box
-            p={8}
-            position='fixed'
-            top='50%'
-            left='50%'
-            transform='translate(-50%, -50%)'
-            zIndex='2'
-            maxWidth='960px'
-            width='100%'
+            onClick={toggleModal}
+            display='block'
+            fontSize='48px'
+            textAlign='right'
+            sx={{ cursor: 'pointer' }}
           >
-            <Box
-              onClick={toggleModal}
-              display='block'
-              fontSize='48px'
-              textAlign='right'
-              sx={{ cursor: 'pointer' }}
-            >
-              &times;
-            </Box>
-
-            <Box
-              bg='white'
-              width='100%'
-              height='100%'
-            >
-              {children}
-            </Box>
+            &times;
           </Box>
-        ) : null
-      }
-    </React.Fragment>
+
+          <Box
+            bg='white'
+            width='100%'
+            height='100%'
+            zIndex={3}
+          >
+            {children}
+          </Box>
+        </Box>
+      </React.Fragment>
+    ) : null
   );
 };
