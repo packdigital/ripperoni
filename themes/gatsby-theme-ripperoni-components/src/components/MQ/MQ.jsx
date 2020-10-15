319c5aa9e31c09c57e2cd70df908febcaa58ea4f
Andrew Nguyen <anguy95@gmail.com>
2020-10-15 13:10:16 -0700

    revert: MQ comp back to what it was before

--- themes/gatsby-theme-ripperoni-components/src/components/MQ/MQ.jsx
+++ themes/gatsby-theme-ripperoni-components/src/components/MQ/MQ.jsx
@@ -1,5 +1,5 @@
-// import React from 'react';
-import { useBreakpointIndex } from '@theme-ui/match-media';
+/** @jsx jsx */
+import { jsx } from 'theme-ui';
 import PropTypes from 'prop-types';
 
 import { useResponsiveProp } from '../../hooks/useResponsiveProp';
@@ -9,43 +9,33 @@ import { MQCsr } from './MQ.Csr';
 export const MQ = ({ comps }) => {
   const mqComps = useResponsiveProp({ initItems: comps });
 
-  // Below is an implementation that adds/removes the component instead of hiding/showing it
-  //
-  const index = useBreakpointIndex();
+  const toUniqueDisplaySxArrays = (sxArr, comp, index) => {
+    sxArr[index] = mqComps.map((mqComp) => {
+      if (mqComp === comp) return 'initial';
+      else return 'none';
+    });
+    return sxArr;
+  };
 
-  return mqComps[index];
+  const toDisplaySxWrapped = (displaySx, index) => {
+    if ( !uniqueComps[index] ) return null;
+    return (
+      <div
+        key={`MQ-${index}-${uniqueComps[index].type.displayName}`}
+        className={`${uniqueComps[index].type.displayName}` || ''}
+        sx={{ ...uniqueComps[index].props.sx, display: displaySx }}
+      >
+        {uniqueComps[index]}
+      </div>
+    );
+  };
 
+  // eslint-disable-next-line no-undef
+  const uniqueComps =  [...new Set(mqComps)];
 
-  // // Below is used because sometimes we render the wrong component initially?
-  // //
-  // const toUniqueDisplaySxArrays = (sxArr, comp, index) => {
-  //   sxArr[index] = mqComps.map((mqComp) => {
-  //     if (mqComp === comp) return 'inherit';
-  //     else return 'none';
-  //   });
-
-  //   return sxArr;
-  // };
-
-  // const toDisplaySxWrapped = (displaySx, index) => {
-  //   if ( !uniqueComps[index] ) return null;
-  //   return (
-  //     <div
-  //       key={`MQ-${index}-${uniqueComps[index].type.displayName}`}
-  //       className={`${uniqueComps[index].type.displayName}` || ''}
-  //       sx={{ ...uniqueComps[index].props.sx, display: displaySx }}
-  //     >
-  //       {uniqueComps[index]}
-  //     </div>
-  //   );
-  // };
-
-  // // eslint-disable-next-line no-undef
-  // const uniqueComps =  [...new Set(mqComps)];
-
-  // return uniqueComps
-  //   .reduce(toUniqueDisplaySxArrays, [])
-  //   .map(toDisplaySxWrapped);
+  return uniqueComps
+    .reduce(toUniqueDisplaySxArrays, [])
+    .map(toDisplaySxWrapped);
 };
 
 MQ.displayName = 'MQ';
