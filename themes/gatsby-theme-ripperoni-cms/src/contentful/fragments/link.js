b9108298ddc225aa335dd538a02d173e59d9ee01
tshamz <tyler@theshamboras.com>
2020-11-11 15:03:04 -0800

    add newWindow to link fragment

--- themes/gatsby-theme-ripperoni-cms/src/contentful/fragments/link.js
+++ themes/gatsby-theme-ripperoni-cms/src/contentful/fragments/link.js
@@ -1,10 +1,10 @@
 import { graphql } from 'gatsby';
 
-
 export const ContentfulAtomLinkFragment = graphql`
   fragment ContentfulAtomLink on ContentfulAtomLink {
     text
     url
+    newWindow
     cms_color: color {
       color: content
     }
