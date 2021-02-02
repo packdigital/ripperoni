8a643c0058b1680b0b77dd0b9d0394f0130c6c61
tshamz <tyler@theshamboras.com>
2021-02-01 16:25:44 -0800

    wait to publish addtocart event

--- themes/gatsby-theme-ripperoni-cart/src/hooks/useAddItemsToCart.js
+++ themes/gatsby-theme-ripperoni-cart/src/hooks/useAddItemsToCart.js
@@ -1,8 +1,24 @@
+import { useEffect, useRef } from 'react';
+
 import { ADD_TO_CART } from '../constants';
 import { useCartContext } from '../context/CartContext';
 
 export const useAddItemsToCart = () => {
-  const { addLineItems, cart, customer, messageBus } = useCartContext();
+  const itemsRef = useRef(null);
+  const addLineItemsLoadingRef = useRef(null);
+  const { addLineItems, cart, state, customer, messageBus } = useCartContext();
+
+  useEffect(() => {
+    const wasLoading = addLineItemsLoadingRef.current === true;
+    const finishedLoading = state.loading.addLineItems === false;
+
+    if (wasLoading && finishedLoading) {
+      const items = itemsRef.current;
+      messageBus?.publish(ADD_TO_CART, { items, cart, customer });
+    }
+
+    addLineItemsLoadingRef.current = state.loading.addLineItems;
+  }, [state.loading.addLineItems]);
 
   const addItemsToCart = (items) => {
     if (items.length < 1) {
@@ -23,9 +39,9 @@ export const useAddItemsToCart = () => {
       }
     });
 
-    addLineItems(items);
+    itemsRef.current = items;
 
-    messageBus?.publish(ADD_TO_CART, { items, cart, customer });
+    addLineItems(items);
   };
 
   return addItemsToCart;
