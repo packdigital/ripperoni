1b8bc3a28ebe980e1c8134cae6236b1517c6b225
tshamz <tyler@theshamboras.com>
2021-06-10 11:24:53 -0700

    prettier auto-styleupdates

--- plugins/gatsby-plugin-301-redirects/gatsby-node.js
+++ plugins/gatsby-plugin-301-redirects/gatsby-node.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 const path = require('path');
-
 const split = require('split');
-
 
 const writeFile = (file, data, reporter) => {
   // eslint-disable-next-line no-undef
-  return new Promise(resolve => {
-    fs.writeFile(file, data, err => {
+  return new Promise((resolve) => {
+    fs.writeFile(file, data, (err) => {
       if (err) {
-        reporter.warn(`301 Redirects: Unable to write to redirects file - ${file}`);
+        reporter.warn(
+          `301 Redirects: Unable to write to redirects file - ${file}`
+        );
       }
 
-      reporter.success(`301 Redirects: Successfully wrote redirects to file - ${file}`);
+      reporter.success(
+        `301 Redirects: Successfully wrote redirects to file - ${file}`
+      );
 
       resolve();
     });
@@ -21,10 +23,12 @@ const writeFile = (file, data, reporter) => {
 
 const readFile = (file, reporter) => {
   // eslint-disable-next-line no-undef
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     fs.readFile(file, 'utf8', (err, data = '') => {
       if (err) {
-        reporter.warn(`301 Redirects: Unable to read existing redirects from file - ${file}`);
+        reporter.warn(
+          `301 Redirects: Unable to read existing redirects from file - ${file}`
+        );
 
         resolve();
       }
@@ -36,26 +40,30 @@ const readFile = (file, reporter) => {
   });
 };
 
-const countLines = filePath => {
+const countLines = (filePath) => {
   // eslint-disable-next-line no-undef
   return new Promise((resolve, reject) => {
     let lineCount = 0;
 
     fs.createReadStream(filePath)
       .pipe(split())
-      .on('data', line => {
+      .on('data', (line) => {
         const isValidLine = /^[a-zA-Z/]/.test(line);
 
         if (isValidLine) lineCount++;
       })
       .on('end', () => resolve(lineCount))
-      .on('error', error => reject(error));
+      .on('error', (error) => reject(error));
   });
 };
 
 exports.onPostBuild = async function onPostBuild(helpers, options) {
   const { getNodesByType, reporter } = helpers;
-  const { redirectsFilePath, redirectsOutputPath, nodeType = 'GoogleSpreadsheet301' } = options;
+  const {
+    redirectsFilePath,
+    redirectsOutputPath,
+    nodeType = 'GoogleSpreadsheet301',
+  } = options;
   const redirectNodes = getNodesByType(nodeType);
 
   try {
@@ -64,17 +72,26 @@ exports.onPostBuild = async function onPostBuild(helpers, options) {
     }
 
     const existingRedirects = await readFile(redirectsFilePath, reporter);
-    const newRedirects = redirectNodes.reduce((redirects, { to, from }) => `${redirects}\n${from} ${to}`, '');
+    const newRedirects = redirectNodes.reduce(
+      (redirects, { to, from }) => `${redirects}\n${from} ${to}`,
+      ''
+    );
     const data = `${newRedirects}\n${existingRedirects}`;
 
     const existingRedirectsCount = await countLines(redirectsFilePath);
     const newRedirectsCount = redirectNodes.length;
 
-    reporter.info(`301 Redirects: Found ${existingRedirectsCount} existing redirects`);
+    reporter.info(
+      `301 Redirects: Found ${existingRedirectsCount} existing redirects`
+    );
     reporter.info(`301 Redirects: Found ${newRedirectsCount} new redirects`);
-    reporter.info(`301 Redirects: Writing out ${existingRedirectsCount + newRedirectsCount} total redirects`);
+    reporter.info(
+      `301 Redirects: Writing out ${
+        existingRedirectsCount + newRedirectsCount
+      } total redirects`
+    );
 
-    return await writeFile(redirectsOutputPath, data, reporter);;
+    return await writeFile(redirectsOutputPath, data, reporter);
   } catch (error) {
     reporter.warn('301 Redirects: Something went wrong');
   }
--- themes/gatsby-theme-ripperoni-components/src/components/Checkbox/Checkbox.jsx
+++ themes/gatsby-theme-ripperoni-components/src/components/Checkbox/Checkbox.jsx
@@ -7,41 +7,33 @@ import { useSxProps } from '../../hooks/useSxProps';
 import { Box } from '../Box';
 import { Flex } from '../Flex';
 
+export const Checkbox = forwardRef(
+  ({ label, variant, defaultChecked, ...incomingProps }, ref) => {
+    const { sxObject, props, propTypes } = useSxProps(incomingProps);
 
-export const Checkbox = forwardRef(({
-  label,
-  variant,
-  defaultChecked,
-  ...incomingProps
-}, ref) => {
-  const { sxObject, props, propTypes } = useSxProps(incomingProps);
+    Checkbox.propTypes = {
+      ...propTypes,
+      label: PropTypes.string,
+      variant: PropTypes.string,
+      defaultChecked: PropTypes.bool,
+    };
 
-  Checkbox.propTypes = {
-    ...propTypes,
-    label: PropTypes.string,
-    variant: PropTypes.string,
-    defaultChecked: PropTypes.bool,
-  };
-
-  return (
-    <Flex
-      data-comp={Checkbox.displayName}
-      as={Label}
-      sx={{
-        alignItems: 'center',
-        ...sxObject,
-      }}
-      variant={variant}
-      className='checkbox'
-    >
-      <CheckboxUI
-        ref={ref}
-        defaultChecked={defaultChecked}
-        {...props}
-      />
-      <Box>{label}</Box>
-    </Flex>
-  );
-});
+    return (
+      <Flex
+        data-comp={Checkbox.displayName}
+        as={Label}
+        sx={{
+          alignItems: 'center',
+          ...sxObject,
+        }}
+        variant={variant}
+        className='checkbox'
+      >
+        <CheckboxUI ref={ref} defaultChecked={defaultChecked} {...props} />
+        <Box>{label}</Box>
+      </Flex>
+    );
+  }
+);
 
 Checkbox.displayName = 'Checkbox';
